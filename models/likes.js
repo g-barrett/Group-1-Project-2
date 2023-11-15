a7f58325a548e1a8f1c5acc287ebdca27e70a878
Drew Tullos <tullos.andrew@gmail.com>
2023-11-14 19:05:13 -0600

    db seeded

--- models/likes.js
+++ models/likes.js
@@ -3,4 +3,26 @@ const sequelize = require('../config/connection.js');
 
 class Likes extends Model {};
 
+const Likes = sequelize.define('Likes', {
+    userId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'User',
+            key: 'id'
+        }
+    },
+    restaurantId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'Restaurant',
+            key: 'id'
+        }
+    }
+});
+
+// Associations
+User.belongsToMany(Restaurant, { through: Likes });
+Restaurant.belongsToMany(User, { through: Likes });
+
+
 Likes.init();
---
+++ seeds/seed.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+const sequelize = require('../config/connection');
+const { User, Restaurant } = require('../models'); 
+
+const seedData = async () => {
+    // Read JSON file
+    // Sync database
+    await sequelize.sync({ force: true });
+    console.log('Database synced successfully!')
+
+    const rawDataUser = fs.readFileSync(path.join(__dirname, 'userSeedData.json'));
+    const users = JSON.parse(rawDataUser);
+    await User.bulkCreate(users, {
+        individualHooks: true,
+        returning: true,
+    })
+    console.log('Users seeded successfully!');
+
+    const rawDataRest = fs.readFileSync(path.join(__dirname, 'restaurantSeedData.json'));
+    const restaurants = JSON.parse(rawDataRest);
+    await Restaurant.bulkCreate(restaurants, {
+        individualHooks: true,
+        returning: true,
+        });
+        console.log('Restaurants seeded successfully!')
+    };
+
+seedData().then(() => {
+    console.log("Data seeded successfully.");
+    }).catch(error => {
+    console.error("Data seeding users:", error);
+    });
