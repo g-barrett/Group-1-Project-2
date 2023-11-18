1cec7d12335b338a72aedbf0745c0987c90ecbf6
Drew Tullos <tullos.andrew@gmail.com>
2023-11-18 16:47:14 -0600

    Fix of map revision 2

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,20 +1,23 @@
-// CLient side JS
-// Renders the Maps to the page
-
-// Code provided by google documentation
-// Initialize and add the map
-
-// Need the search to pull in the geolocation
-// Thought set an array for the add marker to loop through the contnet
-
-
-// Google Autocomplete Search Form
 let autocomplete;
+
+let map;
+
+let lat = 30.266666;
+let lng = -97.733330;
+// let markers = [/*Pass in API info*/];
+
+// Imports API fetchData
+const fetchData = require('../../utils/apiUtil').fetchData;
+
+
+// Generate auto complete
 function initAutocomplete() {
+    console.log('ERROR IS ABOVE ME');
+
     autocomplete = new google.maps.places.Autocomplete(
         document.getElementById('search'),
         {
-            types: ['establishment'],
+            types: ['restaurant', 'cafe', ],
             componentRestrictions: {'country': ['US']},
             fields: ['place_id', 'geometry', 'name']
         }
@@ -24,14 +27,14 @@ function initAutocomplete() {
 }
 
 function onPlaceChanged() {
-    var place = autocomplete.getPlace();
+var place = autocomplete.getPlace();
 
-    if (!place.geometry) {
-        // User did not select a prediction; reset the input field
-        document.getElementById('autocomplete').placeholder = 'Enter a place';
-    } else {
-        // Display details about the valid place
-        document.getElementById('details').innerHTML = place.name;
+if (!place.geometry) {
+// User did not select a prediction; reset the input field
+document.getElementById('autocomplete').placeholder = 'Enter a place';
+} else {
+// Display details about the valid place
+document.getElementById('details').innerHTML = place.name;
     }
 }
 
@@ -45,43 +48,39 @@ function onPlaceChanged() {
 // });
 
 
-let map;
-
-let lat = 30.266666;
-let lng = -97.733330;
 
 let markers = [/*Pass in API info*/];
 
 async function initMap() {
-    // Lat Long variables
-    const position = { lat: lat, lng: lng };
-    //  New map
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 10,
-        center: position
-    });
+// Lat Long variables
+const position = { lat: lat, lng: lng };
+//  New map
+map = new google.maps.Map(document.getElementById('map'), {
+zoom: 10,
+center: position
+});
 
-    function addMarker(props) {
-        const marker = new google.maps.Marker({
-            position: props.coords,
-            map: map,
-            title: 'Uluru'
-            // icon:''
-        });
+function addMarker(props) {
+const marker = new google.maps.Marker({
+position: props.coords,
+map: map,
+title: 'Uluru'
+// icon:''
+});
 
-        if(props.content) {
-            var infoWindow = new google.maps.InfoWindow({
-                content: props.content
-            })
-            marker.addListener('click', function(){
-                infoWindow.open(map, marker);
-            })
-        }
+if(props.content) {
+var infoWindow = new google.maps.InfoWindow({
+content: props.content
+})
+marker.addListener('click', function(){
+infoWindow.open(map, marker);
+})
+}
     }
 
 
     for (let i = 0; i < markers.length; i++ ) {
-        addMarker( markers[i]);
+    addMarker( markers[i]);
     }
     // Template for the map markers
     addMarker({
@@ -97,195 +96,7 @@ async function initMap() {
 
 initMap();
 
-// EXTRA
 
-
-
-
-
-
-
-
-// CARAS CODE
-/*
-// ---GOOGLE MAPS API TO SHOW GAS STATIONS----
-// Start variables for the map, service, and information window
-let map;
-let service;
-let infowindow;
-// Disable fields
-document.getElementById(“diesel”).disabled = true;
-document.getElementById(“gasoline”).disabled = true;
-document.getElementById(“midGrade”).disabled = true;
-document.getElementById(“premium”).disabled = true;
-// Function to show the modal
-function showModal() {
-    const modal = document.getElementById(‘loading-modal’);
-    modal.classList.add(‘is-active’);
-}
-// Function to hide the modal
-function hideModal() {
-    const modal = document.getElementById(‘loading-modal’);
-    modal.classList.remove(‘is-active’);
-}
-// Function to setup the Google Map with provided latitude and longitude
-function initMap(lat, lng) {
-    // Convert user input to Google Maps latitude and longitude object
-    const userInput = new google.maps.LatLng(lat, lng);
-    // Setup the information window (this will display details when a marker is clicked)
-    infowindow = new google.maps.InfoWindow();
-    // Start Google Map centered on the given coordinates and with a zoom level of 15
-    map = new google.maps.Map(document.getElementById(“map-result”), {
-        center: userInput,
-        zoom: 15,
-    });
-    // Construct a request to search for gas stations around the user’s location
-    const request = {
-        location: userInput,
-        radius: ‘500’,
-        query: “Gas station”
-    };
-    // Setup the Places Service to query Google Maps
-    service = new google.maps.places.PlacesService(map);
-    // Perform a text search using the constructed request
-    service.textSearch(request, (results, status) => {
-        // If the search was successful, add markers for each found place
-        if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-            for (let i = 0; i < results.length; i++) {
-                createMarker(results[i]);
-            }
-            // Center the map on the first result
-            map.setCenter(results[0].geometry.location);
-        }
-    });
-}
-// Function to create a clickable marker for a specific place on the map
-function createMarker(place) {
-    // Skip if the place lacks essential details
-    if (!place.name || !place.geometry.location) return;
-    // Create a new marker at the place’s location
-    const marker = new google.maps.Marker({
-        map,
-        position: place.geometry.location,
-        icon: “./assets/images/station-marker.png”
-    });
-    // Attach an event listener to open an info window with the place’s name when the marker is clicked
-    google.maps.event.addListener(marker, “click”, () => {
-        infowindow.setContent(place.name || “”);
-        infowindow.open(map);
-    });
-}
-// ---API TO GET LATITUDE AND LONGITUDE OF A CITY----
-// Function to convert a city’s name into latitude and longitude
-function getLatLngFromCity(city) {
-    // API key and endpoint for Google’s Geocoding service
-    const apiKey = “AIzaSyBxWLcUfDyLG_YtaDZMM5GFhq5rCt7m7EM”;
-    const apiUrl = https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${apiKey};
-    // Fetch the city’s coordinates using the API
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            // If a valid response is received, update the UI and fetch relevant details
-            if (data.results && data.results.length > 0) {
-                const location = data.results[0].geometry.location;
-                document.getElementById(‘latitude’).value = location.lat;
-                document.getElementById(‘longitude’).value = location.lng;
-                initMap(location.lat, location.lng);
-                fetchGasPrices(location.lng, location.lat);
-            }
-        })
-        // Log any errors during the fetch process
-        .catch(error => console.error(“Error fetching geocode data: “, error));
-}
-// Disable the input fields
-document.getElementById(“longitude”).disabled = true;
-document.getElementById(“latitude”).disabled = true;
-// ---API To FETCH GAS PRIZES BY COORDINATES ----
-// Function to fetch gas prices based on given coordinates
-function fetchGasPrices(longitude, latitude) {
-    showModal();  // Show the modal when you start fetching the data
-     // Create a new XMLHttpRequest to fetch data
-     var xhr = new XMLHttpRequest();
-    // Create a new XMLHttpRequest to fetch data
-    var xhr = new XMLHttpRequest();
-    xhr.open(“GET”, https://api.collectapi.com/gasPrice/fromCoordinates?lng=${longitude}&lat=${latitude});
-    // Set necessary headers for the request
-    xhr.setRequestHeader(“content-type”, “application/json”);
-    xhr.setRequestHeader(“authorization”, “apikey 08VbL3jN1gp2ATqNQtFypp:2TsHZX1IoAjbTJgk7nxLML”);
-    // Process the response when data loading is complete
-    xhr.onload = function() {
-        // If the request was successful, update the UI with the received data
-        if (xhr.status === 200) {
-            var response = JSON.parse(xhr.responseText);
-            if (response.success && response.result) {
-                document.getElementById(“diesel”).value = response.result.diesel;
-                document.getElementById(“gasoline”).value = response.result.gasoline;
-                document.getElementById(“midGrade”).value = response.result.midGrade;
-                document.getElementById(“premium”).value = response.result.premium;
-            } else {
-                console.error(“Error in fetching gas prices:“, response.message);
-            }
-        } else {
-            console.error(“Failed to retrieve gas prices.“);
-        }
-    };
-    // Log any errors that occur during the data fetching process
-    xhr.onerror = function() {
-        console.error(“Error occurred while fetching data.“);
-    };
-    // Send the actual request
-    xhr.send();
-    // Automatically hide the modal after 4 seconds
-    setTimeout(hideModal, 4000);
-}
-// Attach event listeners when the document is fully loaded
-document.addEventListener(‘DOMContentLoaded’, function() {
-    // Display user’s previously searched cities
-    renderUserInput();
-    // When the “submit” button is clicked, fetch details for the entered city
-    document.getElementById(‘submit-button’).addEventListener(‘click’, function(event) {
-        event.preventDefault();
-        const city = document.getElementById(‘city’).value;
-        getLatLngFromCity(city);
-    });
-});
-// Function to display the user’s previous city searches from local storage
-function renderUserInput() {
-    let citySearchInput = JSON.parse(localStorage.getItem(‘citySearch’));
-    let cityHistory = document.getElementById(‘user-city’);
-    // If there are past searches, render them on the page
-    if (citySearchInput !== null) {
-        cityHistory.innerHTML = ‘’;
-        citySearchInput.forEach(function(citySearch) {
-            let cityItem = document.createElement(‘p’);
-            cityItem.textContent = citySearch.citySearch;
-            cityHistory.appendChild(cityItem);
-        });
-    }
-}
-// Attach another event listener to the “submit” button to save the entered city in local storage
-document.getElementById(‘submit-button’).addEventListener(‘click’, function(event) {
-    event.preventDefault();
-    let city = document.getElementById(‘city’).value;
-    var citySearch = {
-        citySearch: city,
-    };
-    // Save the search if it’s not empty
-    if (city.trim() !== ‘’) {
-        let citySearchInput = JSON.parse(localStorage.getItem(‘citySearch’)) || [];
-        citySearchInput.push(citySearch);
-        localStorage.setItem(‘citySearch’, JSON.stringify(citySearchInput));
-        renderUserInput();
-    } else {
-        console.error(‘Enter a city name’);
-        return;
-    }
-});
-// Expose the initMap function to the global scope, so it can be accessed outside this script
-window.initMap = initMap;
-*/
-
-/* Gabes code
 // Get current position of the user from browser
 function geoFindMe() {
     const status = document.querySelector("#status");
