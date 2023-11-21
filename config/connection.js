584e1f3418847eeba33183a693541e1628f1499d
Drew Tullos <tullos.andrew@gmail.com>
2023-11-20 19:01:46 -0600

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,22 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    port: 3306,
-    }
-);
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+	sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+	sequelize = new Sequelize(
+		process.env.DB_NAME,
+		process.env.DB_USER,
+		process.env.DB_PASSWORD,
+		{
+			host: "127.0.0.1",
+			dialect: "mysql",
+			port: 3306,
+		}
+	);
+}
 
 module.exports = sequelize;
 
@@ -18,4 +24,3 @@ module.exports = sequelize;
 // sequelize.authenticate()
 //     .then(() => console.log('Database connected.'))
 //     .catch(err => console.error('Unable to connect to the database:', err));
-
