e4b90d91f8b7f82bd10a13819f45339f24b23e1a
g-barrett <barrettg516@gmail.com>
2023-11-20 18:47:36 -0600

    adjustments to models and associations

--- models/likes.js
+++ models/likes.js
@@ -3,28 +3,34 @@ const sequelize = require('../config/connection.js');
 
 class Likes extends Model {};
 
-const Likes = sequelize.define('Likes', {
-    user_id: {
+
+
+// Associations
+// User.hasMany(Restaurant, { through: Likes });
+// Restaurant.belongsToMany(User, { through: Likes });
+
+
+Likes.init(
+    {
+        user_id: {
         type: DataTypes.INTEGER,
         references: {
             model: 'User',
             key: 'id'
-        }
-    },
+            }
+        },
     restaurant_id: {
         type: DataTypes.INTEGER,
         references: {
-            model: 'Restaurant',
+            model: 'Restaurants',
             key: 'id'
-        }
+            }
+        },
+    },
+    {
+        sequelize,
+        modelName: 'likes',
     }
-});
-
-// Associations
-User.hasMany(Restaurant, { through: Likes });
-Restaurant.belongsToMany(User, { through: Likes });
-
-
-Likes.init();
+);
 
 module.exports = Likes;
